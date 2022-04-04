5f7b12883a1c8ba05539bf90bea46bfd363a486c
MacLoki <newbeeloki@gmail.com>
2022-04-05 02:46:34 +0800

    feat(git): add commitizen
    使用 commitizen 规范代码提交

--- vue3-admin-system/.eslintrc.js
+++ vue3-admin-system/.eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
+  // ? 表示当前目录即为根目录，ESLint 规则将被限制到该目录下
   root: true,
+  // ? env 表示启用 ESLint 检测的环境
   env: {
+    // ? 在 node 环境下启动 ESLint 检测
     node: true
   },
+  // ? ESLint 中基础配置需要继承的配置
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard'
@@ -10,16 +14,10 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
+  // ? 启用规则及各自的错误级别
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    indent: 0,
-    'comma-dangle': ['error', 'never'],
-    'space-before-function-paren': 0,
-    'vue/multi-word-component-names': ['error', {
-      ignores: [
-        'index'
-      ]
-    }]
+    'space-before-function-paren': 'off'
   }
 }
