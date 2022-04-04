59452388dc92bbd7a09abad55e35a1711f730e90
MacLoki <newbeeloki@gmail.com>
2022-04-04 23:43:11 +0800

    refactor: 重新开始项目

--- vue3-admin-system/src/api/login.js
+++
@@ -1,9 +0,0 @@
-import request from './request'
-
-export const login = data => {
-  return request({
-    url: '/login',
-    method: 'POST',
-    data
-  })
-}
--- vue3-admin-system/src/store/getters.js
+++
@@ -1,3 +0,0 @@
-export default {
-  token: state => state.app.token
-}
